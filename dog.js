cd261fc5fa9a375324dfc1410bdd55ac760a66fc
vu thanh hieu <vuthanhieu2000@gmail.com>
2019-11-28 10:52:49 +0700

    new method sayHi for dog

--- dog.js
+++ dog.js
@@ -1,3 +1,11 @@
-function dog(){
-    
+var Chalk = require('chalk');
+
+function dog(name){
+    this.name = name;
 }
+
+dog.prototype.sayHi = function () {
+    console.log('Hello I is a dog, My name is ' + Chalk.red(this.name));
+}
+
+module.exports = dog;
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Mouse = require('./mouse');
 var Cat = require('./cat');
-
+var Dog = require('./dog');
 var micky = new Cat();
 
 
@@ -9,3 +9,6 @@ var  mouse1 = new Mouse("Dog", 19);
 micky.eat(mouse1);
 mouse1.die();
 micky.show(mouse1);
+
+var dog = new Dog('Tom');
+dog.sayHi();
